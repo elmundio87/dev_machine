6d6b80737954806954ef4e4c4306c256b5e4ea8f
Edmund Dipple <elmundio1987@gmail.com>
2014-11-20 17:00:28 +0000

    blah2

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ xfconf-query -c xfce4-panel -p /panels/panel-0/size -s 48
 #map the windows key to the "start menu"
 xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/Super_L" -n -t string -s "xfce4-popup-whiskermenu"
 
+#Add dockbarx to the panel
+xfce4-panel --add=dockbarx
+
 
 
 #Start Rubymine
